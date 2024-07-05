24152930a512c240a693d70d2c053fb60e9b4dda
dullat <dullat@users.noreply.github.com>
2024-07-06 00:52:08 +0530

    base working

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ import Root, {
 } from "./componants/Root"
 import Notes from "./componants/Notes"
 import Note, { loader as noteLoader } from "./componants/Note"
+import Edit, { action as editAction } from "./componants/Edit"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" loader={rootLoader} action={rootAction} element={<Root />}>
       <Route index element={<Notes />} />
       <Route path="note/:id" loader={noteLoader} element={<Note />} />
+      <Route path="note/:id/edit" action={editAction} element={<Edit />} />
     </Route>
   )
 )
--- src/componants/Edit.jsx
+++ src/componants/Edit.jsx
@@ -1,32 +1,92 @@
-import { Form } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Form, redirect, useParams } from "react-router-dom"
 
 export async function action({ request, params }) {
   try {
-    const res = await fetch(`https://localhost:3000/`)
+    const formData = await request.formData()
+    const updates = Object.fromEntries(formData)
+    console.log(updates)
+    const res = await fetch(`http://localhost:3000/notes/${params.id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updates),
+    })
+
+    if (!res.ok) {
+      throw new Error("Failed to update note")
+    }
+
+    return redirect(`/note/${params.id}`)
   } catch (error) {
-    error
+    return error
   }
 }
 
 const Edit = () => {
+  const [note, setNote] = useState()
+  const [loading, setLoading] = useState(true)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getNote = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/notes/${id}`)
+        const note = await res.json()
+
+        setNote(note)
+        console.log(note)
+        setLoading(false)
+      } catch (error) {
+        return error
+      }
+    }
+
+    getNote()
+  }, [id])
   return (
     <div>
       <p>Update Note</p>
-      <Form>
-        <div>
-          <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
-        </div>
-        <div>
-          <label htmlFor="des">Description</label>
-          <input type="text" name="des" id="des" />
-        </div>
-        <div>
-          <label htmlFor="text">Title</label>
-          <input type="text" name="text" id="text" />
-        </div>
-        <button type="submit">btn</button>
-      </Form>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <Form method="post">
+          <div>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder={note.title}
+            />
+          </div>
+          <div>
+            <label htmlFor="date">Date</label>
+            <input type="text" name="date" id="date" placeholder={note.date} />
+          </div>
+          <div>
+            <label htmlFor="des">Description</label>
+            <input type="text" name="des" id="des" placeholder={note.des} />
+          </div>
+          <div>
+            <label htmlFor="text">Text</label>
+            <input type="text" name="text" id="text" placeholder={note.text} />
+          </div>
+          <div>
+            <label htmlFor="id">ID</label>
+            <input
+              type="text"
+              name="id"
+              id="id"
+              placeholder={note.id}
+              disabled
+            />
+          </div>
+          <button type="submit">btn</button>
+        </Form>
+      )}
     </div>
   )
 }
